9a0a16201a87cd7b81ea0a86c0c8abe91e29d21e
kirubasar <kiruba.sri.official@gmail.com>
2024-04-21 23:19:30 +0530

    initial commit

---
+++ Anonymous function/anonymousfun.js
@@ -0,0 +1,120 @@
+console.log("1.Print Odd Numbers in an Array:");
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const printOddNumbers = function(arr) {
+    for (let ind in arr) {
+        if (array[ind]% 2 !== 0) {
+            console.log(array[ind]);
+    
+        }
+    }
+};
+printOddNumbers(array);
+console.log("--------------------");
+console.log("2.Convert all the Strings to Title Caps in a String Array:");
+const stringsArray = ["all the best", "hard work is the best way of success", "never lose"];
+const titleCapsArray = [];
+
+const convertToTitleCaps = function(array) {
+    for (let str of array) {
+        //console.log(str)
+        let titleCaseString = "";
+        let capitalizeNext = true;
+
+        for (let char of str) {
+            //console.log(char)
+            if (capitalizeNext) {
+                titleCaseString += char.toUpperCase();
+                capitalizeNext = false;
+            } else {
+                titleCaseString += char.toLowerCase();
+            }
+            if (char === ' ') {
+                capitalizeNext = true;
+            }
+        }
+
+        titleCapsArray.push(titleCaseString);
+    }
+};
+convertToTitleCaps(stringsArray);
+console.log(titleCapsArray);
+console.log("--------------------");
+console.log("3.Sum of all Numbers in an Array:");
+const array1 = [10, 24, 90, 45, 59];
+
+const sum = function(arr) {
+    let total = 0;
+    for (let num=0; num<array1.length; num++) {
+        total += array1[num];
+    }
+    return total;
+};
+
+console.log(sum(array1)); 
+console.log("--------------------");
+console.log("4.Return all the Prime Numbers in an Array:")
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const primeNumbers = numbers.filter(function(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+});
+
+console.log(primeNumbers);
+console.log("--------------------");
+console.log("5.Return all the Palindromes in an Array:");
+const arr = ["level", "hello", "radar", "world", "civic"];
+
+const palindromes = arr.filter(function(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+});
+
+console.log(palindromes);
+console.log("--------------------");
+console.log("6.Return Median of Two Sorted Arrays of the Same Sfize:")
+const medianValues = function(arr2, arr3)  {
+    const mergedArray = arr2.concat(arr3).sort((a, b) => a - b);
+    const mid = (arr2.length + arr3.length) / 2;
+    return (mergedArray[Math.floor(mid)] + mergedArray[Math.ceil(mid)]) / 2;
+};
+const array2 = [1, 2, 3, 8];
+const array3 = [9, 5, 7, 4];
+
+console.log(medianValues(array2, array3));
+console.log("--------------------");
+console.log("7.Remove Duplicates from an Array:")
+let num = [1, 2, 3, 4, 4, 5, 6, 6, 19, 19, 30];
+let uniqueArr = function(arr) {
+    let seen = {};
+    let result = [];
+    for (let i in arr) {
+        if (!seen[arr[i]]) {
+            seen[arr[i]] = true;
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
+console.log(uniqueArr(num)); 
+console.log("--------------------");
+console.log("8.Rotate an Array by k Times")
+const rotateArrayByanonymous = function(arr, k)  {
+    const rotatedArray = [];
+    const len = arr.length;
+    k = k % len;
+    for (let i = 0; i < len; i++) {
+        rotatedArray[(i + k) % len] = arr[i];
+    }
+
+    return rotatedArray;
+};
+const array4 = [1, 2, 3, 4, 5, 9, 6, 10];
+const k = 4;
+console.log("Original array4:", array4);
+console.log("Rotated array by k (anonymous function):", rotateArrayByanonymous(array4, k));
+console.log("--------------------");
---
+++ Arrow funtion/arrowfun.js
@@ -0,0 +1,74 @@
+console.log("1.Print Odd Numbers in an Array:");
+let array = [1, 4, 5, 7, 9, 10, 11]
+const printOddNumbers = arr => {
+    for(num of array){
+        if(num % 2 !== 0)
+        console.log(num)
+    }
+}
+printOddNumbers(array);
+console.log("--------------------");
+console.log("2.Sum of all Numbers in an Array");
+const stringsArray = ["hello world", "good morning", "how are you"];
+const titleCapsArray = [];
+
+const convertToTitleCaps = (arr) => {
+    for (let str of arr) {
+        let titleCaseString = "";
+        let capitalizeNext = true;
+
+        for (let char of str) {
+            if (capitalizeNext) {
+                titleCaseString += char.toUpperCase();
+                capitalizeNext = false;
+            } else {
+                titleCaseString += char.toLowerCase();
+            }
+            if (char === ' ') {
+                capitalizeNext = true;
+            }
+        }
+
+        titleCapsArray.push(titleCaseString);
+    }
+};
+
+convertToTitleCaps(stringsArray);
+console.log(titleCapsArray);
+console.log("--------------------"); 
+console.log("3.Sum of all Numbers in an Array")
+const array1 = [1, 2, 3, 40, 55];
+
+const sum = (arr)=> {
+    let total = 0;
+    for (let num=0; num<array1.length; num++) {
+        total += array1[num];
+    }
+    return total;
+};
+
+console.log(sum(array1));
+console.log("--------------------");
+console.log("4.Return all the Prime Numbers in an Array")
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 19, 23];
+
+const primeNumbers = numbers.filter((num)=> {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+});
+
+console.log(primeNumbers);
+console.log("--------------------");
+console.log("5.Return all the Palindromes in an Array:");
+const arr = ["level", "hello", "radar", "world", "civic"];
+
+const palindromes = arr.filter((word)=> {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+});
+
+console.log(palindromes);
+console.log("--------------------");
